c19375a318e3ec9c4814494da607e9b1b8a52c7a
Jaimie de Vries <49305844+jaimiedevries@users.noreply.github.com>
2022-04-29 12:06:01 +0200

    functies herschreven en verplaatst
    Bezig geweest om bepaalde functies beter leesbaar te maken door ze op te knippen. Enige twijfel punt is de functie 'toggleTemperatuurSchaal' die kan volgens mij nog veel cleaner.

--- dist/index.8f0c9192.js
+++ dist/index.8f0c9192.js
@@ -3,8 +3,40 @@ let locationButton = document.getElementById('locationBtn');
 let locationString = document.getElementById("location");
 let celsius;
 let fahrenheit;
+locationButton.addEventListener('click', locatieOphalen);
+//   Bij een klik op de container van de toggle buttons wordt de toggleTemperatuurSchaal getriggerd.
+toggleButtons.addEventListener('click', toggleTemperatuurSchaal);
+/* Bij een klik op de 'locatie' knop start geolocation met draaien, wanneer er een locatie is gevonden 
+pakken we de breedtegraad en lengtegraad en zetten ze in de API url. Hierdoor krijgen we weergegevens uit de locatie
+om weerdata te tonen op de pagina zetten we toonLokaleWeerGegevens in nadat we de data hebben ontvangen.  */ function locatieOphalen() {
+    if (!navigator.geolocation) locationString.textContent = "Geolocation wordt niet door je browser ondersteund";
+    else {
+        locationString.textContent = "Locatie ophalen...";
+        navigator.geolocation.getCurrentPosition(locatieOntvangen, locatieNietOntvangen);
+    }
+}
+function locatieNietOntvangen() {
+    locationString.textContent = "Kan je locatie niet ophalen";
+}
+function locatieOntvangen(coordinaten) {
+    const breedteGraad = coordinaten.coords.latitude;
+    const lengteGraad = coordinaten.coords.longitude;
+    const api_url = `https://fcc-weather-api.glitch.me/api/current?lat=${breedteGraad}&lon=${lengteGraad}`;
+    ontvangLokaleWeerGegevens(api_url);
+}
+async function ontvangLokaleWeerGegevens(data) {
+    const response = await fetch(data);
+    const weerData = await response.json();
+    toonLokaleWeerGegevens(weerData);
+}
 // Functie die weerdata vanuit de locatieOntvangen functie plaatst op de webpagina.
-function toonLokaleWeerGegevens(data) {
+function toonLokaleWeerGegevens(weerData) {
+    plaatsDataInHTML(weerData);
+    toggleBackgroundThema();
+    // De togglebuttons om te switchen van celsius naar fahrenheit is beschikbaar nadat alle data op de pagina is getoond.
+    toggleButtons.style.display = "flex";
+}
+function plaatsDataInHTML(data) {
     let temperature = document.getElementById("temperature");
     let description = document.getElementById("weather-description");
     celsius = Math.round(data.main.temp);
@@ -12,51 +44,27 @@ function toonLokaleWeerGegevens(data) {
     temperature.textContent = celsius + "℃";
     description.textContent = data.weather[0].description;
     locationString.textContent = data.name + ", " + data.sys.country;
-    // De togglebuttons om te switchen van celsius naar fahrenheit is beschikbaar nadat alle data op de pagina is getoond.
-    toggleButtons.style.display = "flex";
-    /*  Wanneer het warmer is dan 16 graden celsius dan tonen we een 'hot' theme en anders 'cold'
-  middels de data attribute op de HTML pagina. */ if (celsius >= 16) document.documentElement.setAttribute('data-theme', 'hot');
-    else document.documentElement.setAttribute('data-theme', 'cold');
-    //   Bij een klik op de container van de toggle buttons wordt de toggleTemperatuurSchaal getriggerd.
-    toggleButtons.addEventListener('click', toggleTemperatuurSchaal);
+}
+/*  Wanneer het warmer is dan 16 graden celsius dan tonen we een 'hot' theme en anders 'cold'
+  middels de data attribute op de HTML pagina. */ function toggleBackgroundThema() {
+    if (celsius >= 16) setThema('hot');
+    else setThema('cold');
 }
 /* Bekijkt of de click heeft plaatsgevonden op een element zonder de class 'active' én welke id het bevat.
 Wanneer dit waar is krijgt het element waar op is geklikt de class 'active' en wordt de class 'active' van een vorige of
-volgende element in de parent verwijderd. */ function toggleTemperatuurSchaal(e) {
-    if (e.target.classList[1] !== 'active' && e.target.id === 'btn-celsius') {
-        e.target.classList.add('active');
+volgende element in de parent verwijderd. */ function toggleTemperatuurSchaal(event) {
+    if (event.target.classList[1] !== 'active' && event.target.id === 'btn-celsius') {
+        event.target.classList.add('active');
         temperature.textContent = celsius + '℃';
-        e.target.nextElementSibling.classList.remove('active');
-    } else if (e.target.classList[1] !== 'active' && e.target.id === 'btn-fahrenheit') {
-        e.target.classList.add('active');
+        event.target.nextElementSibling.classList.remove('active');
+    } else if (event.target.classList[1] !== 'active' && event.target.id === 'btn-fahrenheit') {
+        event.target.classList.add('active');
         temperature.textContent = fahrenheit + '℉';
-        e.target.previousElementSibling.classList.remove('active');
+        event.target.previousElementSibling.classList.remove('active');
     }
 }
-/* 
-Bij een klik op de 'locatie' knop start geolocation met draaien, wanneer er een locatie is gevonden 
-pakken we de breedtegraad en lengtegraad en zetten ze in de API url. Hierdoor krijgen we weergegevens uit de locatie
-om weerdata te tonen op de pagina zetten we toonLokaleWeerGegevens in nadat we de data hebben ontvangen.  */ function locatieOphalen() {
-    function locatieNietOntvangen() {
-        locationString.textContent = "Kan je locatie niet ophalen";
-    }
-    function locatieOntvangen(locatie) {
-        const breedteGraad = locatie.coords.latitude;
-        const lengteGraad = locatie.coords.longitude;
-        const api_url = `https://fcc-weather-api.glitch.me/api/current?lat=${breedteGraad}&lon=${lengteGraad}`;
-        async function ontvangLokaleWeerGegevens() {
-            const response = await fetch(api_url);
-            const weerData = await response.json();
-            toonLokaleWeerGegevens(weerData);
-        }
-        ontvangLokaleWeerGegevens();
-    }
-    if (!navigator.geolocation) locationString.textContent = "Geolocation wordt niet door je browser ondersteund";
-    else {
-        locationString.textContent = "Locatie ophalen...";
-        navigator.geolocation.getCurrentPosition(locatieOntvangen, locatieNietOntvangen);
-    }
+function setThema(themaNaam) {
+    document.documentElement.setAttribute('data-theme', themaNaam);
 }
-locationButton.addEventListener('click', locatieOphalen);
 
 //# sourceMappingURL=index.8f0c9192.js.map
--- src/js/app.js
+++ src/js/app.js
@@ -5,72 +5,14 @@ let locationString = document.getElementById("location");
 let celsius;
 let fahrenheit;
 
-// Functie die weerdata vanuit de locatieOntvangen functie plaatst op de webpagina.
-function toonLokaleWeerGegevens(data) {
-  let temperature = document.getElementById("temperature");
-  let description = document.getElementById("weather-description");
-
-  celsius = Math.round(data.main.temp);
-  fahrenheit = Math.trunc(celsius * 1.8 + 32);
-
-  temperature.textContent = celsius + "℃";
-  description.textContent = data.weather[0].description
-  locationString.textContent = data.name + ", " + data.sys.country;
-
-  // De togglebuttons om te switchen van celsius naar fahrenheit is beschikbaar nadat alle data op de pagina is getoond.
-  toggleButtons.style.display = "flex";
-
- /*  Wanneer het warmer is dan 16 graden celsius dan tonen we een 'hot' theme en anders 'cold'
-  middels de data attribute op de HTML pagina. */
-  if (celsius >= 16) {
-    document.documentElement.setAttribute('data-theme', 'hot');
-  } else {
-    document.documentElement.setAttribute('data-theme', 'cold');
-  }
-
+locationButton.addEventListener('click', locatieOphalen);
 //   Bij een klik op de container van de toggle buttons wordt de toggleTemperatuurSchaal getriggerd.
-  toggleButtons.addEventListener('click', toggleTemperatuurSchaal);
-}
+toggleButtons.addEventListener('click', toggleTemperatuurSchaal);
 
-/* Bekijkt of de click heeft plaatsgevonden op een element zonder de class 'active' én welke id het bevat.
-Wanneer dit waar is krijgt het element waar op is geklikt de class 'active' en wordt de class 'active' van een vorige of
-volgende element in de parent verwijderd. */
-function toggleTemperatuurSchaal(e){
-  if(e.target.classList[1] !== 'active' && e.target.id === 'btn-celsius'){
-    e.target.classList.add('active');
-    temperature.textContent = celsius + '℃';
-    e.target.nextElementSibling.classList.remove('active');
-  } else if (e.target.classList[1] !== 'active' && e.target.id === 'btn-fahrenheit'){
-    e.target.classList.add('active');
-    temperature.textContent = fahrenheit + '℉';
-    e.target.previousElementSibling.classList.remove('active');
-  }
-}
-/* 
-Bij een klik op de 'locatie' knop start geolocation met draaien, wanneer er een locatie is gevonden 
+/* Bij een klik op de 'locatie' knop start geolocation met draaien, wanneer er een locatie is gevonden 
 pakken we de breedtegraad en lengtegraad en zetten ze in de API url. Hierdoor krijgen we weergegevens uit de locatie
 om weerdata te tonen op de pagina zetten we toonLokaleWeerGegevens in nadat we de data hebben ontvangen.  */
-function locatieOphalen(){
-
-  function locatieNietOntvangen() {
-    locationString.textContent = "Kan je locatie niet ophalen";
-  }
-
-  function locatieOntvangen(locatie) {
-    const breedteGraad = locatie.coords.latitude;
-    const lengteGraad = locatie.coords.longitude;
-    const api_url = `https://fcc-weather-api.glitch.me/api/current?lat=${breedteGraad}&lon=${lengteGraad}`;
-  
-    async function ontvangLokaleWeerGegevens() {
-      const response = await fetch(api_url);
-      const weerData = await response.json();
-  
-      toonLokaleWeerGegevens(weerData);
-    }
-  
-    ontvangLokaleWeerGegevens();
-  }
-  
+function locatieOphalen() {
   if (!navigator.geolocation) {
     locationString.textContent = "Geolocation wordt niet door je browser ondersteund";
   } else {
@@ -80,7 +22,69 @@ function locatieOphalen(){
       locatieNietOntvangen
     );
   }
-
 }
 
-locationButton.addEventListener('click', locatieOphalen);
+function locatieNietOntvangen() {
+  locationString.textContent = "Kan je locatie niet ophalen";
+}
+
+function locatieOntvangen(coordinaten) {
+  const breedteGraad = coordinaten.coords.latitude;
+  const lengteGraad = coordinaten.coords.longitude;
+  const api_url = `https://fcc-weather-api.glitch.me/api/current?lat=${breedteGraad}&lon=${lengteGraad}`;
+  ontvangLokaleWeerGegevens(api_url);
+}
+
+async function ontvangLokaleWeerGegevens(data) {
+  const response = await fetch(data);
+  const weerData = await response.json();
+  toonLokaleWeerGegevens(weerData);
+}
+// Functie die weerdata vanuit de locatieOntvangen functie plaatst op de webpagina.
+function toonLokaleWeerGegevens(weerData) {
+  plaatsDataInHTML(weerData);
+  toggleBackgroundThema();
+  // De togglebuttons om te switchen van celsius naar fahrenheit is beschikbaar nadat alle data op de pagina is getoond.
+  toggleButtons.style.display = "flex";
+}
+
+function plaatsDataInHTML(data) {
+  let temperature = document.getElementById("temperature");
+  let description = document.getElementById("weather-description");
+
+  celsius = Math.round(data.main.temp);
+  fahrenheit = Math.trunc(celsius * 1.8 + 32);
+
+  temperature.textContent = celsius + "℃";
+  description.textContent = data.weather[0].description
+  locationString.textContent = data.name + ", " + data.sys.country;
+}
+
+/*  Wanneer het warmer is dan 16 graden celsius dan tonen we een 'hot' theme en anders 'cold'
+  middels de data attribute op de HTML pagina. */
+  function toggleBackgroundThema() {
+    if (celsius >= 16) {
+      setThema('hot');
+    } else {
+      setThema('cold');
+    }
+  }
+
+/* Bekijkt of de click heeft plaatsgevonden op een element zonder de class 'active' én welke id het bevat.
+Wanneer dit waar is krijgt het element waar op is geklikt de class 'active' en wordt de class 'active' van een vorige of
+volgende element in de parent verwijderd. */
+function toggleTemperatuurSchaal(event) {
+  if (event.target.classList[1] !== 'active' && event.target.id === 'btn-celsius') {
+    event.target.classList.add('active');
+    temperature.textContent = celsius + '℃';
+    event.target.nextElementSibling.classList.remove('active');
+  } else if (event.target.classList[1] !== 'active' && event.target.id === 'btn-fahrenheit') {
+    event.target.classList.add('active');
+    temperature.textContent = fahrenheit + '℉';
+    event.target.previousElementSibling.classList.remove('active');
+  }
+}
+
+function setThema (themaNaam) {
+  document.documentElement.setAttribute('data-theme', themaNaam);
+}
